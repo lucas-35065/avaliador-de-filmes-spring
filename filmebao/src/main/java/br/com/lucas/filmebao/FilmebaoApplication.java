30f2910b55dc6e8c8df2468d8ef326f5b399692d
Lucas Rodrigues <lucas.rodrigues.35065@gmail.com>
2024-05-01 19:29:55 +0000

    feat: busca por temporada

--- filmebao/src/main/java/br/com/lucas/filmebao/FilmebaoApplication.java
+++ filmebao/src/main/java/br/com/lucas/filmebao/FilmebaoApplication.java
@@ -1,5 +1,8 @@
 package br.com.lucas.filmebao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +11,7 @@ import br.com.lucas.filmebao.service.ConsumoApi;
 import br.com.lucas.filmebao.service.ConverteDados;
 import br.com.lucas.filmebao.model.DadosEpisodio;
 import br.com.lucas.filmebao.model.DadosSerie;
+import br.com.lucas.filmebao.model.DadosTemporada;
 
 @SpringBootApplication
 public class FilmebaoApplication implements CommandLineRunner {
@@ -20,13 +24,22 @@ public class FilmebaoApplication implements CommandLineRunner {
 @Override
 public void run(String... args) throws Exception {
 	var consumoApi = new ConsumoApi();
-	var json = consumoApi.obterDados("http://www.omdbapi.com/?t=supernatural&apikey=85adf8ba");
+	var json = consumoApi.obterDados("http://www.omdbapi.com/?t=bojack+horseman&apikey=85adf8ba");
 	var conversor = new ConverteDados();
 	DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
 	System.out.println(dadosSerie);
-	json = consumoApi.obterDados("http://www.omdbapi.com/?t=supernatural&season=1&episode=2&apikey=85adf8ba");	
+	json = consumoApi.obterDados("http://www.omdbapi.com/?t=bojack+horseman&season=1&episode=2&apikey=85adf8ba");	
 	DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 	System.out.println(dadosEpisodio);
+
+	List<DadosTemporada> listaTemporadas = new ArrayList<>();
+
+	for(int i = 1; i <= dadosSerie.totalTemporadas(); i++){
+		json = consumoApi.obterDados("http://www.omdbapi.com/?t=bojack+horseman&season=" + i + "&apikey=85adf8ba");
+		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+		listaTemporadas.add(dadosTemporada);
+	}
+	listaTemporadas.forEach(System.out::println);
 }
 
 }
--- filmebao/src/main/java/br/com/lucas/filmebao/model/DadosTemporada.java
+++ filmebao/src/main/java/br/com/lucas/filmebao/model/DadosTemporada.java
@@ -1,5 +1,12 @@
 package br.com.lucas.filmebao.model;
 
-public record DadosTemporada() {
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties (ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Season") Integer numeroTemporada,
+                             @JsonAlias("Episodes") List <DadosEpisodio> episodios) {
 
 }
